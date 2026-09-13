872c3b712dcf1de6d66f6c09bd750f6ce34c9b28
mrudkowski <rudkowski.dev@gmail.com>
2026-09-13 11:14:12 +0200

    add vue page structure

---
+++ app/components/HeroSection.vue
@@ -0,0 +1,9 @@
+<template>
+  <section class="section bg-gray-900 text-gray-100">
+    <div class="container-page">
+      <div class="w-full">
+        <p>Dupa</p>
+      </div>
+    </div>
+  </section>
+</template>
---
+++ app/layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex min-h-screen flex-col">
+    <header class="flex min-h-18 items-center justify-between gap-6">
+
+    </header>
+
+    <main class="flex-1">
+      <slot />
+    </main>
+
+    <footer class="flex min-h-18 items-center">
+
+    </footer>
+  </div>
+</template>
